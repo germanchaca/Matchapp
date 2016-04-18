6f1cc6e415643ac937e044867d1bb77e01b5c91c
germanchaca <germanchaca@gmail.com>
2016-04-17 22:34:06 -0300

    Cardstack

---
+++ app/src/main/java/fiuba/matchapp/adapter/SwipeDeckAdapter.java
@@ -0,0 +1,71 @@
+package fiuba.matchapp.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import fiuba.matchapp.R;
+
+/**
+ * Created by german on 4/17/2016.
+ */
+public class SwipeDeckAdapter extends BaseAdapter {
+
+    private List<String> data;
+    private Context context;
+
+    public SwipeDeckAdapter(List<String> data, Context context) {
+        this.data = data;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+        if (v == null) {
+            v = LayoutInflater.from(parent.getContext()).inflate(R.layout.candidate_card, parent, false);
+        }
+        ImageView imageView = (ImageView) v.findViewById(R.id.offer_image);
+        Picasso.with(context).load(R.drawable.user_dummy_thumbnail_male).fit().centerCrop().into(imageView);
+        TextView textView = (TextView) v.findViewById(R.id.sample_text);
+        String item = (String)getItem(position);
+        textView.setText(item);
+
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Layer type: ", Integer.toString(v.getLayerType()));
+                Log.i("Hwardware Accel type:", Integer.toString(View.LAYER_TYPE_HARDWARE));
+                //Intent i = new Intent(v.getContext(), BlankActivity.class);
+                //v.getContext().startActivity(i);
+            }
+        });
+        return v;
+    }
+}
--- app/src/main/java/fiuba/matchapp/fragment/Connect.java
+++ app/src/main/java/fiuba/matchapp/fragment/Connect.java
@@ -2,17 +2,29 @@ package fiuba.matchapp.fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import java.util.ArrayList;
+
+import fiuba.cardstack.SwipeDeck;
 import fiuba.matchapp.R;
+import fiuba.matchapp.adapter.SwipeDeckAdapter;
 import fiuba.matchapp.helper.RippleAnimation;
 
 public class Connect extends Fragment {
+    private static final String TAG = "Connect_fragment";
     RippleAnimation rippleBackground1;
+    private SwipeDeck cardStack;
+    private SwipeDeckAdapter adapter;
+    private ArrayList<String> testData;
 
+    Button btnSwipeLeft;
+    Button btnSwipeRight;
+    Button btnAddCandidate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,44 +37,120 @@ public class Connect extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_connect, container, false);
 
-        init(view);// to initialize widgets
+        init(view);
 
-        doRippleBackground(); //start ripple background work..
+        startAnimation();
+
+        initCardstack();
+        stopAnimation();
+        //stopAnimation();//deberia estar cuando recibo el http request answer con los candidatos
         return view;
     }
 
+    private void agregarCandidatosDePrueba(){
+        testData.add("0");
+        testData.add("1");
+        testData.add("2");
+        testData.add("3");
+        testData.add("4");
+
+    }
+    private void initCardstack(){
+        testData = new ArrayList<>();
+        agregarCandidatosDePrueba();
+
+        adapter = new SwipeDeckAdapter(testData, getActivity());
+        cardStack.setAdapter(adapter);
+        cardStack.setEventCallback(new SwipeDeck.SwipeEventCallback() {
+            @Override
+            public void cardSwipedLeft(int position) {
+                Log.i("MainActivity", "card was swiped left, position in adapter: " + position);
+            }
+
+            @Override
+            public void cardSwipedRight(int position) {
+                Log.i("MainActivity", "card was swiped right, position in adapter: " + position);
+            }
+
+            @Override
+            public void cardsDepleted() {
+                Log.i("MainActivity", "no more cards");
+                startAnimation();
+                //TODO aca va de nuevo la petición al http server, ojo que si supera el max hay que mostrar otra pantalla
+            }
+
+            @Override
+            public void cardActionDown() {
+                Log.i(TAG, "cardActionDown");
+            }
+
+            @Override
+            public void cardActionUp() {
+                Log.i(TAG, "cardActionUp");
+            }
+
+        });
+    }
     private void init(View view) {
         rippleBackground1 = (RippleAnimation) view.findViewById(R.id.content);
         rippleBackground1.setVisibility(View.VISIBLE);
-    }
 
-    public void doRippleBackground() {
+        cardStack = (SwipeDeck) view.findViewById(R.id.swipe_deck);
+        cardStack.setHardwareAccelerationEnabled(true);
 
-        //start ripple background animations
-        startAnimation();
+        cardStack.setLeftImage(R.id.left_image);
+        cardStack.setRightImage(R.id.right_image);
 
-        //handler created to handle cardStack as well as timer...
-        new Handler().postDelayed(new Runnable() {
+        btnSwipeLeft = (Button) view.findViewById(R.id.button);
+        btnSwipeLeft.setVisibility(View.GONE);
+        btnSwipeLeft.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                //termina el efecto
-            }
-        }, 3000);
+            public void onClick(View v) {
+                cardStack.swipeTopCardLeft(180);
 
+            }
+        });
+
+        btnSwipeRight= (Button) view.findViewById(R.id.button2);
+
+        btnSwipeRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardStack.swipeTopCardRight(180);
+            }
+        });
+
+        btnAddCandidate = (Button) view.findViewById(R.id.button3);
+        btnAddCandidate.setVisibility(View.GONE);
+        btnAddCandidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                testData.add("a sample string.");
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     private void startAnimation() {
         //if it's not running
         if (!rippleBackground1.isRippleAnimationRunning()) {
+            rippleBackground1.setVisibility(View.VISIBLE);
+            btnSwipeLeft.setVisibility(View.GONE);
+            btnSwipeRight.setVisibility(View.GONE);
+            btnAddCandidate.setVisibility(View.GONE);
             rippleBackground1.startRippleAnimation();
         }
     }
 
-    //this method will stop background ripple animation. if it's running.
     private void stopAnimation() {
         if (rippleBackground1.isRippleAnimationRunning()) {
             rippleBackground1.stopRippleAnimation();
+            rippleBackground1.setVisibility(View.GONE);
+            btnSwipeLeft.setVisibility(View.VISIBLE);
+            btnSwipeRight.setVisibility(View.VISIBLE);
+            btnAddCandidate.setVisibility(View.VISIBLE);
         }
+
     }
 
 
