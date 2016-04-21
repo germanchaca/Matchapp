1bda619bb9234d1ae79026ff94f37041ff110eec
germanchaca <germanchaca@gmail.com>
2016-04-21 17:00:48 -0300

    Editable Profile Activity

--- app/src/main/java/fiuba/matchapp/gcm/GcmIntentService.java
+++ app/src/main/java/fiuba/matchapp/gcm/GcmIntentService.java
@@ -8,14 +8,27 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.gcm.GcmPubSub;
 import com.google.android.gms.gcm.GoogleCloudMessaging;
 import com.google.android.gms.iid.InstanceID;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import fiuba.matchapp.R;
 import fiuba.matchapp.app.Config;
+import fiuba.matchapp.app.MyApplication;
+import fiuba.matchapp.app.RestAPIContract;
+import fiuba.matchapp.model.User;
 
 /**
  * Created by german on 4/21/2016.
@@ -68,7 +81,7 @@ public class GcmIntentService extends IntentService {
             Log.e(TAG, "GCM Registration Token: " + token);
 
             // sending the registration id to our server
-            sendRegistrationToServer(token);
+            //sendRegistrationToServer(token);
 
             sharedPreferences.edit().putBoolean(Config.SENT_TOKEN_TO_SERVER, true).apply();
         } catch (Exception e) {
@@ -80,12 +93,6 @@ public class GcmIntentService extends IntentService {
         Intent registrationComplete = new Intent(Config.REGISTRATION_COMPLETE);
         registrationComplete.putExtra("token", token);
         LocalBroadcastManager.getInstance(this).sendBroadcast(registrationComplete);
-    }
-
-    private void sendRegistrationToServer(final String token) {
-        // Send the registration token to our server
-        // to keep it in MySQL
-
     }
 
     /**
@@ -129,4 +136,62 @@ public class GcmIntentService extends IntentService {
         }
     }
 
+    private void sendRegistrationToServer(final String token) {
+        // checking for valid login session
+        User user = MyApplication.getInstance().getPrefManager().getUser();
+        if (user == null) {
+            //  Si no se encuentra usuario, redireccionar a loginScreen.
+            return;
+        }
+        String endPoint = RestAPIContract.USER.replace("_ID_", user.getId());
+
+        //se hace la modificación del registration gsm Id en el app server
+        StringRequest strReq = new StringRequest(Request.Method.PUT,
+                endPoint, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                Log.e(TAG, "response: " + response);
+
+                try {
+                    JSONObject obj = new JSONObject(response);
+
+                    // check for error
+                    if (obj.getBoolean("error") == false) {
+                        // broadcasting token sent to server
+                        Intent registrationComplete = new Intent(Config.SENT_TOKEN_TO_SERVER);
+                        LocalBroadcastManager.getInstance(getApplicationContext()).sendBroadcast(registrationComplete);
+                    } else {
+                        Log.e(TAG, getResources().getString(R.string.gsmintentservice_error_server_app_send_registrationgsm) );
+                        //Toast.makeText(getApplicationContext(), getResources().getString(R.string.gsmintentservice_error_server_app_send_registrationgsm) + obj.getJSONObject("error").getString("message"), Toast.LENGTH_LONG).show();
+                    }
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "json parsing error: " + e.getMessage());
+                    //Toast.makeText(getApplicationContext(), "Json parse error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                Log.e(TAG, "Volley error: " + error.getMessage() + ", code: " + networkResponse);
+                //Toast.makeText(getApplicationContext(), "Volley error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("gcm_registration_id", token);
+
+                Log.e(TAG, "params: " + params.toString());
+                return params;
+            }
+        };
+
+        //Adding request to request queue
+        MyApplication.getInstance().addToRequestQueue(strReq);
+    }
 }
