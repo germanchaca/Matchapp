ed2ab73689112e5cdcd27a99b06e2734fe1c395d
germanchaca <germanchaca@gmail.com>
2016-04-26 17:41:52 -0300

    LoginActivity- SignUpActivity - facebook sdk

--- app/src/main/java/fiuba/matchapp/activity/FacebookLoginActivity.java
+++ app/src/main/java/fiuba/matchapp/activity/FacebookLoginActivity.java
@@ -1,8 +1,13 @@
 package fiuba.matchapp.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,6 +18,8 @@ import com.facebook.FacebookSdk;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 /**
@@ -25,6 +32,7 @@ public abstract class FacebookLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //android.util.Log.e("HashKey:", printHashKey(this));
         FacebookSdk.sdkInitialize(getApplicationContext());
 
         callbackManager = CallbackManager.Factory.create();
@@ -67,5 +75,20 @@ public abstract class FacebookLoginActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+    public static String printHashKey(Context ctx) {
+        try {
+            PackageInfo info = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                return Base64.encodeToString(md.digest(), Base64.DEFAULT);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            return "SHA-1 generation: the key count not be generated: NameNotFoundException thrown";
+        } catch (NoSuchAlgorithmException e) {
+            return "SHA-1 generation: the key count not be generated: NoSuchAlgorithmException thrown";
+        }
 
+        return "SHA-1 generation: epic failed";
+    }
 }
