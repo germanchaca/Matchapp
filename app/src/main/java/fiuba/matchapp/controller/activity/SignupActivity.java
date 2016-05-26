980811c0dfd80f51016124b850f32edf58d8a9f1
germanchaca <germanchaca@gmail.com>
2016-05-25 23:59:21 -0300

    JSON correciones

--- app/src/main/java/fiuba/matchapp/controller/activity/SignupActivity.java
+++ app/src/main/java/fiuba/matchapp/controller/activity/SignupActivity.java
@@ -35,6 +35,8 @@ import fiuba.matchapp.model.User;
 import fiuba.matchapp.controller.fragment.DatePickerFragment;
 import fiuba.matchapp.controller.clickToSelectEditText.ClickToSelectEditText;
 import fiuba.matchapp.controller.clickToSelectEditText.Item;
+import fiuba.matchapp.networking.BaseRequest;
+import fiuba.matchapp.networking.BaseStringRequest;
 import fiuba.matchapp.networking.JSONmetadata;
 import fiuba.matchapp.networking.JsonObjectGen;
 import fiuba.matchapp.networking.JsonParser;
@@ -60,7 +62,7 @@ public class SignupActivity extends AppCompatActivity {
     private int age;
     private String fbId;
     private Boolean hasFbId;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,36 +76,49 @@ public class SignupActivity extends AppCompatActivity {
         _loginLink = (TextView) findViewById(R.id.link_login);
         _sex_input = (ClickToSelectEditText<Item>) findViewById(R.id.sex_input);
 
+        initConfirmSignInButton();
+        initLoginLinkButton();
+        initSexInput();
+        inicializarConCuentaFacebook();
 
+    }
+
+    private void initConfirmSignInButton() {
         _signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signup();
+                try {
+                    signup();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
+    }
 
+    private void initLoginLinkButton() {
         _loginLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
 
+    private void initSexInput() {
         ArrayList<Item> lstSexos = new ArrayList<Item>();
 
         String[] sexos = getResources().getStringArray(R.array.sex_array);
 
-        for (int i = 0; i < sexos.length ; i++){
+        for (int i = 0; i < sexos.length; i++) {
             Item iSexo = new Item(sexos[i]);
             lstSexos.add(iSexo);
         }
         _sex_input.setItems(lstSexos);
-        inicializarConCuentaFacebook();
-
     }
 
 
-    public void inicializarConCuentaFacebook(){
+    public void inicializarConCuentaFacebook() {
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             userName = extras.getString("userName");
@@ -117,17 +132,18 @@ public class SignupActivity extends AppCompatActivity {
             _nameText.setText(userName);
             _sex_input.setText(gender);
             hasFbId = true;
-        }else{
+        } else {
             hasFbId = false;
         }
     }
+
     public void showDatePickerDialog(View v) {
         DatePickerFragment newFragment = new DatePickerFragment();
         newFragment.setEditText(_dateText);
         newFragment.show(getFragmentManager(), "datePicker");
     }
 
-    public void signup() {
+    public void signup() throws JSONException {
         Log.d(TAG, "Signup");
 
         if (!validate()) {
@@ -151,7 +167,137 @@ public class SignupActivity extends AppCompatActivity {
         password = _passwordText.getText().toString();
         //aca tambien mandar el fbImageUrl al server
 
-        StringRequest strReq = new StringRequest(Request.Method.POST,
+        HashMap<String, String> headers = new HashMap<String, String>();
+        headers.put("content-type", "application/json");
+
+        HashMap<String, String> params = new HashMap<>();
+
+        JSONObject userJson = new JSONObject();
+        userJson.put("name", userName);
+        userJson.put("alias", userName);
+        userJson.put("email", email);
+        userJson.put("sex", gender);
+        userJson.put("age", age);
+        JSONArray interestsJsonArray = new JSONArray();
+        userJson.put("interests", interestsJsonArray);
+        userJson.put("photo_profile", "");
+        userJson.put("password", MD5.getHashedPassword(password));
+        JSONObject locationJson = JsonObjectGen.getJsonObjectFromLocation(0, 0);
+        userJson.put("location", locationJson);
+        userJson.put("gcm_registration_id", FirebaseInstanceId.getInstance().getToken());
+
+        params.put("user", userJson.toString());
+
+        JSONObject metadataJson = JSONmetadata.getMetadata(1);
+        params.put("metadata", metadataJson.toString());
+
+        Log.d(TAG, "params: " + params.toString());
+
+        Response.Listener<JSONObject> response = new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "Server Response: " + response);
+                progressDialog.dismiss();
+                User loggedUser = JsonParser.getUserFromJSONresponse(response);
+                String appServerToken = JsonParser.getAppServerTokenFromJSONresponse(response);
+
+                if (loggedUser != null) {
+                    MyApplication.getInstance().getPrefManager().storeUser(loggedUser);
+                }
+                if(appServerToken != null){
+                    MyApplication.getInstance().getPrefManager().storeAppServerToken(appServerToken);
+                }
+                onSignupSuccess();
+            }
+        };
+
+        Response.ErrorListener errorListener = new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                NetworkResponse networkResponse = error.networkResponse;
+                String errorMessage = null;
+                if (error instanceof NoConnectionError) {
+                    errorMessage = getResources().getString(R.string.internet_problem);
+                } else {
+                    errorMessage = getResources().getString(R.string.signup_failed);
+                }
+
+                Log.e(TAG, "Volley error: " + error.getMessage() + ", code: " + networkResponse);
+                onSignupFailed(errorMessage);
+                progressDialog.dismiss();
+            }
+        };
+
+        BaseRequest strReq2 = new BaseRequest(RestAPIContract.POST_USER, params, response, errorListener, Request.Method.POST);
+        BaseRequest strReq3 = new BaseRequest(RestAPIContract.POST_USER, params, headers, response, errorListener, Request.Method.POST);
+        MyApplication.getInstance().addToRequestQueue(strReq2);
+    }
+
+    public void onSignupSuccess() {
+
+
+        _signupButton.setEnabled(true);
+        setResult(RESULT_OK, null);
+
+        Intent intent = new Intent(this, Intro.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void onSignupFailed(String errorMessage) {
+        Toast.makeText(getBaseContext(), errorMessage, Toast.LENGTH_LONG).show();
+        _signupButton.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
+        String birthDate = _dateText.getText().toString();
+        String gender = _sex_input.getText().toString();
+
+        if (name.isEmpty()) {
+            _nameText.setError(getResources().getString(R.string.invalid_name_empty));
+            valid = false;
+        } else {
+            _nameText.setError(null);
+        }
+
+        if (birthDate.isEmpty()) {
+            _dateText.setError(getResources().getString(R.string.invalid_birthdate));
+            valid = false;
+        } else {
+            _dateText.setError(null);
+        }
+        if (gender.isEmpty()) {
+            _sex_input.setError(getResources().getString(R.string.invalid_gender));
+            valid = false;
+        } else {
+            _sex_input.setError(null);
+        }
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            _emailText.setError(getResources().getString(R.string.invalid_mail_invalid));
+            valid = false;
+        } else {
+            _emailText.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            _passwordText.setError(getResources().getString(R.string.invalid_password_format));
+            valid = false;
+        } else {
+            _passwordText.setError(null);
+        }
+
+        return valid;
+    }
+}
+
+        /* StringRequest strReq = new StringRequest(Request.Method.POST,
                 RestAPIContract.POST_USER, new Response.Listener<String>() {
 
             @Override
@@ -233,75 +379,10 @@ public class SignupActivity extends AppCompatActivity {
 
 
         };
-
-        MyApplication.getInstance().addToRequestQueue(strReq);
     }
-
-
-    public void onSignupSuccess() {
-        /*User user;
+    /*User user;
         user = new User("0", userName, userName, email,birthday,gender);
         if (hasFbId){
             user.setFbId(this.fbId);
         }
         MyApplication.getInstance().getPrefManager().storeUser(user);*/
-
-        _signupButton.setEnabled(true);
-        setResult(RESULT_OK, null);
-
-        Intent intent = new Intent(this, Intro.class);
-        startActivity(intent);
-        finish();
-    }
-
-    public void onSignupFailed(String errorMessage) {
-        Toast.makeText(getBaseContext(),errorMessage, Toast.LENGTH_LONG).show();
-        _signupButton.setEnabled(true);
-    }
-
-    public boolean validate() {
-        boolean valid = true;
-
-        String name = _nameText.getText().toString();
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-        String birthDate = _dateText.getText().toString();
-        String gender = _sex_input.getText().toString();
-
-        if (name.isEmpty()) {
-            _nameText.setError(getResources().getString(R.string.invalid_name_empty));
-            valid = false;
-        } else {
-            _nameText.setError(null);
-        }
-
-        if (birthDate.isEmpty()) {
-            _dateText.setError(getResources().getString(R.string.invalid_birthdate));
-            valid = false;
-        } else {
-            _dateText.setError(null);
-        }
-        if (gender.isEmpty()) {
-            _sex_input.setError(getResources().getString(R.string.invalid_gender));
-            valid = false;
-        } else {
-            _sex_input.setError(null);
-        }
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError(getResources().getString(R.string.invalid_mail_invalid));
-            valid = false;
-        } else {
-            _emailText.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError(getResources().getString(R.string.invalid_password_format));
-            valid = false;
-        } else {
-            _passwordText.setError(null);
-        }
-
-        return valid;
-    }
-}
--- app/src/main/java/fiuba/matchapp/networking/BaseStringRequest.java
+++ app/src/main/java/fiuba/matchapp/networking/BaseStringRequest.java
@@ -1,7 +1,87 @@
 package fiuba.matchapp.networking;
 
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by german on 5/25/2016.
  */
-public class BaseStringRequest {
+public class BaseStringRequest extends Request<String> {
+
+    private Response.Listener<String> listener;
+    private Map<String, String> params;
+    private HashMap<String, String> headers;
+    private String body;
+
+    public BaseStringRequest(String url, Map<String, String> params,
+                             Response.Listener<String> reponseListener, Response.ErrorListener errorListener, int method) {
+        super(method, url, errorListener);
+
+        this.setShouldCache(Boolean.FALSE);
+
+        this.listener = reponseListener;
+        this.params = params;
+    }
+
+    public BaseStringRequest(String url, Map<String, String> params, HashMap<String, String> headers,
+                             Response.Listener<String> reponseListener, Response.ErrorListener errorListener, int method){
+        this(url, params, reponseListener, errorListener, method);
+        this.headers = headers;
+    }
+
+    public BaseStringRequest(String url, HashMap<String, String> headers, String body,
+                             Response.Listener<String> reponseListener, Response.ErrorListener errorListener, int method){
+        this(url, null, reponseListener, errorListener, method);
+        this.headers = headers;
+        this.body = body;
+    }
+
+
+    protected Map<String, String> getParams() {
+        return params;
+    };
+
+    @Override
+    public byte[] getBody() throws AuthFailureError {
+        byte[] bodyArray =  this.body.getBytes(Charset.forName("UTF-8"));
+        return bodyArray;
+    }
+
+    @Override
+    public HashMap<String, String> getHeaders() {
+        if(headers == null) headers = new HashMap<String, String>();
+        return headers;
+    }
+
+    @Override
+    protected Response<String> parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString = new String(response.data,
+                    HttpHeaderParser.parseCharset(response.headers));
+            Log.e("Response", jsonString);
+            return Response.success(jsonString,
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        }
+    }
+    @Override
+    public String getBodyContentType() {
+        return "application/json; charset=utf-8";
+    }
+    @Override
+    protected void deliverResponse(String response) {
+        listener.onResponse(response);
+    }
 }
