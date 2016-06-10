6f70dd6ef8f2340c49b7277ed59b6aa4178eac4c
german <germanchaca@gmail.com>
2016-06-09 23:27:06 -0300

    gcm

--- app/src/main/java/fiuba/matchapp/networking/gcm/MyGcmPushReceiver.java
+++ app/src/main/java/fiuba/matchapp/networking/gcm/MyGcmPushReceiver.java
@@ -61,7 +61,7 @@ public class MyGcmPushReceiver extends FirebaseMessagingService {
 
         Map<String, String> data = message.getData();
 
-        //processDataPayload(data, notificationBody, notificationTitle, notifitacionTimestamp );
+        processDataPayload(data, notificationBody, notificationTitle, notifitacionTimestamp );
     }
 
     private void processDataPayload( Map<String, String> data, String notificationBody, String notificationTitle, String notifitacionTimestamp ) {
@@ -73,40 +73,79 @@ public class MyGcmPushReceiver extends FirebaseMessagingService {
         switch (type) {
             case Config.PUSH_TYPE_NEW_MESSAGE:
 
-                String userId = data.get("user_id");
-                String chatMessage = data.get("message");
-                String chatMessageId = data.get("message_id");
-                String chatMessageCreatedAt = data.get("created_at");
-                String chatMessageChatRoomId = data.get("chat_room_id");
-
-                if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
-                    // app is in foreground, broadcast the push message
-                    Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
-
-                    pushNotification.putExtra("type", Config.PUSH_TYPE_NEW_MESSAGE);
-                    pushNotification.putExtra("user_id", userId);
-                    pushNotification.putExtra("message", chatMessage);
-                    pushNotification.putExtra("message_id", chatMessageId);
-                    pushNotification.putExtra("created_at", chatMessageCreatedAt);
-                    pushNotification.putExtra("chat_room_id", chatMessageChatRoomId);
-
-                    LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
-
-                } else {
-                    // app is in background
-
-                    Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
-                    resultIntent.putExtra("message", notificationBody);//habria que darle todos los extras para que se abra el chatRoomActivity
-
-                    showNotificationMessage(getApplicationContext(), notificationTitle, notificationBody, notifitacionTimestamp, resultIntent);
-                }
+                processChatNewMessageNotification(data, notificationBody, notificationTitle, notifitacionTimestamp);
+                break;
+            case Config.PUSH_TYPE_NEW_MATCH:
+                processNewMatchReceived(data,notificationBody,notificationTitle,notifitacionTimestamp);
                 break;
         }
+    }
+
+    private void processNewMatchReceived(Map<String, String> data, String notificationBody, String notificationTitle, String notifitacionTimestamp) {
+        String userId = data.get("user_id");
+        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+            pushNotification.putExtra("type", Config.PUSH_TYPE_NEW_MATCH);
+            pushNotification.putExtra("user_id", userId);
+
+            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+            NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+            notificationUtils.playNotificationSound();
+        }else {
+            Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+            resultIntent.putExtra("type", Config.PUSH_TYPE_NEW_MATCH);
+            resultIntent.putExtra("user_id", userId);
+            showNotificationMessage(getApplicationContext(), notificationTitle, notificationBody, notifitacionTimestamp, resultIntent);
+        }
+    }
+
+    private void processChatNewMessageNotification(Map<String, String> data, String notificationBody, String notificationTitle, String notifitacionTimestamp) {
+        String userId = data.get("user_id");
+        String chatMessage = data.get("message");
+        //String chatMessageId = data.get("message_id");
+        String chatMessageCreatedAt = data.get("created_at");
+        String chatMessageChatRoomId = data.get("chat_room_id");
+
+        if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+            // app is in foreground, broadcast the push message
+            Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+
+            pushNotification.putExtra("type", Config.PUSH_TYPE_NEW_MESSAGE);
+            pushNotification.putExtra("user_id", userId);
+            pushNotification.putExtra("message", chatMessage);
+            //pushNotification.putExtra("message_id", chatMessageId);
+            pushNotification.putExtra("created_at", chatMessageCreatedAt);
+            pushNotification.putExtra("chat_room_id", chatMessageChatRoomId);
+
+            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+            NotificationUtils notificationUtils = new NotificationUtils(getApplicationContext());
+            notificationUtils.playNotificationSound();
+        } else {
+            // app is in background
+
+            Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+            resultIntent.putExtra("message", notificationBody);//habria que darle todos los extras para que se abra el chatRoomActivity
+            resultIntent.putExtra("type", Config.PUSH_TYPE_NEW_MESSAGE);
+            resultIntent.putExtra("user_id", userId);
+            resultIntent.putExtra("message", chatMessage);
+            //resultIntent.putExtra("message_id", chatMessageId);
+            resultIntent.putExtra("created_at", chatMessageCreatedAt);
+            resultIntent.putExtra("chat_room_id", chatMessageChatRoomId);
+
+            showNotificationMessage(getApplicationContext(), notificationTitle, notificationBody, notifitacionTimestamp, resultIntent);
+        }
+        return;
     }
 
     private void showNotificationMessage(Context context, String title, String message, String timeStamp, Intent intent) {
         notificationUtils = new NotificationUtils(context);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         notificationUtils.showNotificationMessage(title, message, timeStamp, intent);
+    }
+
+    private void showNotificationMessageWithBigImage(Context context, String title, String message, String timeStamp, Intent intent, String imageUrl) {
+        notificationUtils = new NotificationUtils(context);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        notificationUtils.showNotificationMessage(title, message, timeStamp, intent, imageUrl);
     }
 }
