b10f106957e83d67b95a5dab80c975417369d38c
german <germanchaca@gmail.com>
2016-06-19 00:32:59 -0300

    correcciones para que ande

--- app/src/main/java/fiuba/matchapp/networking/httpRequests/PutUpdateUserData.java
+++ app/src/main/java/fiuba/matchapp/networking/httpRequests/PutUpdateUserData.java
@@ -210,7 +210,7 @@ public abstract class PutUpdateUserData {
                 } catch (UnsupportedEncodingException e) {
                     e.printStackTrace();
                 }
-                onAppServerDefaultError();
+                make();
                 return;
             }
         };
